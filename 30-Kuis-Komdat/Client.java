33a3a945eb386934e84881089f89d3656fd26d64
Muli eriska <mulieriska14@gmail.com>
2015-12-23 18:28:54 +0700

    Komdat

--- 30-Kuis-Komdat/Client.java
+++ 30-Kuis-Komdat/Client.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 public class Client {
     public void chat() 
                 throws UnknownHostException, IOException {
-        Socket socket = new Socket("192.168.43.191", 33333);
+        Socket socket = new Socket("127.0.0.1", 33333);
         
         try {
             // Ketik
